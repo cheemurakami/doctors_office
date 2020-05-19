c1c99d02f8491e0aff6653cf6a82275771ed0985
Jozy Kinnaman <jozypants@gmail.com>
2020-05-19 13:47:29 -0700

    update method for .all.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,7 +10,18 @@ attr_accessor(:id, :name, :specialty)
     @specialty = attributes.fetch(:specialty)
   end
 
-  
+  def self.all 
+    returned_doctors = DB.execute("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      id = doctor.fetch("id").to_i
+      specialty = doctor.fetch("specialty")
+      doctors.push(Doctor.new({name: name, id: id, :specialty: specialty}))
+    end
+    doctors
+  end
+
 
   def save
     result = DB.exec("INSERT INTO doctors (name) VALUES ('#{@name}') RETURNING id;")
